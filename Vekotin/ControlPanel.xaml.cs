5f6703dc8caa7276eaa9b194db5083f35676081b
N0mad300 <arthur.raffoux@gmail.com>
2025-10-12 00:52:31 +0200

    Added new metadata to widget manifest

--- Vekotin/ControlPanel.xaml.cs
+++ Vekotin/ControlPanel.xaml.cs
@@ -137,15 +137,18 @@ namespace Vekotin
 
     public class WidgetListItem
     {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public WidgetManifest Manifest { get; set; }
+        public string? Name { get; set; }
+        public required string Path { get; set; }
+        public required WidgetManifest Manifest { get; set; }
     }
 
     public class WidgetManifest
     {
-        public string Name { get; set; }
-        public string Version { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Version { get; set; }
+        public string? License { get; set; }
+        public string? Description { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
     }
